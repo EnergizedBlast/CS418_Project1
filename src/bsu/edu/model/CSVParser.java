61df706926b48427d9c88ae8a87d03b01b40ff72
BuildTools <unconfigured@null.spigotmc.org>
2019-08-23 14:49:10 -0400

    Add writer to CSVParser class

--- src/bsu/edu/model/CSVParser.java
+++ src/bsu/edu/model/CSVParser.java
@@ -1,4 +1,12 @@
 package bsu.edu.model;
 
+import java.io.FileWriter;
+import java.io.Writer;
+
 public class CSVParser {
+
+public void writeTo() throws Exception {
+    Writer writer = new FileWriter(".../test.csv");
+}
+
 }
